669330f99028895c9683facd483246a72eeb17fc
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-05-14 21:18:53 -0500

    add words, basic flow

--- cpp-words/main.cpp
+++ cpp-words/main.cpp
@@ -8,7 +8,75 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-  std::cout << "cpp-words" << std::endl;
+// forward delcarations
+bool promptToContinue();
+std::string getAfter(std::string word);
+std::string getBefore(std::string word);
+std::string getUsersGuess();
+std::string pickWord();
+void endGame();
+void showHints(std::string word);
+void showResult(std::string word, std::string guess);
+void updateScore(std::string word, std::string guess, int score);
+
+bool promptToContinue()
+{
+  return false;
+}
+
+std::string getAfter(std::string word)
+{
+  return "after";
+}
+
+std::string getBefore(std::string word)
+{
+  return "before";
+}
+
+std::string getUsersGuess()
+{
+  return "guess";
+}
+
+std::string pickWord()
+{
+  return "word";
+}
+
+void endGame()
+{
+}
+
+void showHints(std::string word)
+{
+  std::string before = getBefore(word);
+  std::string after = getAfter(word);
+}
+
+void showResult(std::string word, std::string guess)
+{
+}
+
+void updateScore(std::string word, std::string guess, int score)
+{
+}
+
+int main(int argc, const char * argv[])
+{
+  int score = 0;
+  while (1)
+  {
+    std::string word = pickWord();
+    showHints(word);
+    std::string guess = getUsersGuess();
+    showResult(word, guess);
+    updateScore(word, guess, score);
+    if (!promptToContinue())
+    {
+      endGame();
+      return 1;
+    }
+  }
   return 0;
 }
