72ca9ddf894482e03bb774c175043597b58da29d
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-05-14 21:59:18 -0500

    use word bank and real hints

--- cpp-words/main.cpp
+++ cpp-words/main.cpp
@@ -7,17 +7,96 @@
 //
 
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 
 // forward delcarations
 bool promptToContinue();
-std::string getAfter(std::string word);
-std::string getBefore(std::string word);
+std::string getAfter(int wordIndex);
+std::string getBefore(int wordIndex);
 std::string getUsersGuess();
-std::string pickWord();
+int pickWordIndex();
 void endGame();
-void showHints(std::string word);
+void showHints(int wordIndex);
 void showResult(std::string word, bool isCorrect);
 int updateScore(bool goodGuess, int score);
+
+std::string wordBank [73] =
+{
+  "and",
+  "and_eq",
+  "asm",
+  "auto",
+  "bitand",
+  "bitor",
+  "bool",
+  "break",
+  "case",
+  "catch",
+  "char",
+  "class",
+  "compl",
+  "const",
+  "const_cast",
+  "continue",
+  "default",
+  "delete",
+  "do",
+  "double",
+  "dynamic_cast",
+  "else",
+  "enum",
+  "explicit",
+  "extern",
+  "false",
+  "float",
+  "for",
+  "friend",
+  "goto",
+  "if",
+  "inline",
+  "int",
+  "long",
+  "mutable",
+  "namespace",
+  "new",
+  "not",
+  "not_eq",
+  "operator",
+  "or",
+  "or_eq",
+  "private",
+  "protected",
+  "public",
+  "register",
+  "reinterpret_cast",
+  "return",
+  "short",
+  "signed",
+  "sizeof",
+  "static",
+  "static_cast",
+  "struct",
+  "switch",
+  "template",
+  "this",
+  "throw",
+  "true",
+  "try",
+  "typedef",
+  "typeid",
+  "typename",
+  "union",
+  "unsigned",
+  "using",
+  "virtual",
+  "void",
+  "volatile",
+  "wchar_t:wchar_t",
+  "while",
+  "xor",
+  "xor_eq"
+};
 
 bool promptToContinue()
 {
@@ -27,14 +106,20 @@ bool promptToContinue()
   return response != "y";
 }
 
-std::string getAfter(std::string word)
+std::string getAfter(int wordIndex)
 {
-  return "after";
+  if (wordIndex < 72) {
+    return wordBank[wordIndex + 1];
+  }
+  return "-";
 }
 
-std::string getBefore(std::string word)
+std::string getBefore(int wordIndex)
 {
-  return "before";
+  if (wordIndex > 0) {
+    return wordBank[wordIndex - 1];
+  }
+  return "-";
 }
 
 std::string getUsersGuess()
@@ -44,9 +129,9 @@ std::string getUsersGuess()
   return input;
 }
 
-std::string pickWord()
+int pickWordIndex()
 {
-  return "word";
+  return rand() % 73;
 }
 
 void endGame()
@@ -54,10 +139,10 @@ void endGame()
   std::cout << "Ciao!" << std::endl;
 }
 
-void showHints(std::string word)
+void showHints(int wordIndex)
 {
-  std::string before = getBefore(word);
-  std::string after = getAfter(word);
+  std::string before = getBefore(wordIndex);
+  std::string after = getAfter(wordIndex);
   std::cout << before << std::endl;
   std::cout << "?" << std::endl;
   std::cout << after << std::endl;
@@ -85,13 +170,15 @@ int updateScore(bool goodGuess, int score)
 int main(int argc, const char * argv[])
 {
   std::cout << "cpp-words" << std::endl;
+  srand((int) time(NULL));
   int score = 0;
   bool goodGuess;
   while (1)
   {
     std::cout << std::endl;
-    std::string word = pickWord();
-    showHints(word);
+    int wordIndex = pickWordIndex();
+    std::string word = wordBank[wordIndex];
+    showHints(wordIndex);
     std::string guess = getUsersGuess();
     goodGuess = guess == word;
     showResult(word, goodGuess);
