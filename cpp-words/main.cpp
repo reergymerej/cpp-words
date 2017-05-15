ab9f90bc0f1015a155aacafd279fd34a60f952bc
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-05-14 20:50:15 -0500

    Initial Commit

---
+++ cpp-words/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp-words
+//
+//  Created by Jeremy Greer on 5/14/17.
+//  Copyright © 2017 Grizzle. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
