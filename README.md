8dc22780037c6128a9a3f39cd899ea65f56b22cd
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-05-14 20:52:51 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# cpp-words
+
+Learn the words.
+
+http://ktutorials.com/cplusplus/keywords-and-reserved-words-in-cplusplus/
--- cpp-words/main.cpp
+++ cpp-words/main.cpp
@@ -9,7 +9,6 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+  std::cout << "cpp-words" << std::endl;
+  return 0;
 }
