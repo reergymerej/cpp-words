8647f8b3ad73e7cc0f9e93500daecd68523540e2
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-05-14 21:39:53 -0500

    fill out functions, handle gameloop/quitting

--- cpp-words/main.cpp
+++ cpp-words/main.cpp
@@ -16,12 +16,15 @@ std::string getUsersGuess();
 std::string pickWord();
 void endGame();
 void showHints(std::string word);
-void showResult(std::string word, std::string guess);
-void updateScore(std::string word, std::string guess, int score);
+void showResult(std::string word, bool isCorrect);
+int updateScore(bool goodGuess, int score);
 
 bool promptToContinue()
 {
-  return false;
+  std::cout << "Quit? (y/n): ";
+  std::string response;
+  std::cin >> response;
+  return response != "y";
 }
 
 std::string getAfter(std::string word)
@@ -36,7 +39,9 @@ std::string getBefore(std::string word)
 
 std::string getUsersGuess()
 {
-  return "guess";
+  std::string input;
+  std::cin >> input;
+  return input;
 }
 
 std::string pickWord()
@@ -46,32 +51,51 @@ std::string pickWord()
 
 void endGame()
 {
+  std::cout << "Ciao!" << std::endl;
 }
 
 void showHints(std::string word)
 {
   std::string before = getBefore(word);
   std::string after = getAfter(word);
+  std::cout << before << std::endl;
+  std::cout << "?" << std::endl;
+  std::cout << after << std::endl;
 }
 
-void showResult(std::string word, std::string guess)
+void showResult(std::string word, bool isCorrect)
 {
+  if (isCorrect)
+  {
+    std::cout << "correct" << std::endl;
+  }
+  else
+  {
+    std::cout << "wrong, it is \"" << word << "\"" << std::endl;
+  }
 }
 
-void updateScore(std::string word, std::string guess, int score)
+int updateScore(bool goodGuess, int score)
 {
+  int newScore = goodGuess ? score + 1 : score - 1;
+  std::cout << "score: " << newScore << std::endl;
+  return newScore;
 }
 
 int main(int argc, const char * argv[])
 {
+  std::cout << "cpp-words" << std::endl;
   int score = 0;
+  bool goodGuess;
   while (1)
   {
+    std::cout << std::endl;
     std::string word = pickWord();
     showHints(word);
     std::string guess = getUsersGuess();
-    showResult(word, guess);
-    updateScore(word, guess, score);
+    goodGuess = guess == word;
+    showResult(word, goodGuess);
+    score = updateScore(goodGuess, score);
     if (!promptToContinue())
     {
       endGame();
